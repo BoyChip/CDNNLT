3043990d051d78f9a7922196120ded55e2f59a95
BoyChip <boys.ngokcamau@gmail.com>
2016-05-08 16:26:28 +0700

    V2.1

--- FinalProject/FinalProject/Business/HocLucBLL.cs
+++ FinalProject/FinalProject/Business/HocLucBLL.cs
@@ -25,28 +25,38 @@ namespace FinalProject.Business
         public int Insert(HocLucEntities obj)
         {
             int result = 0;
-            string strQuery = "insert into dbo.hocluc (MAHOCSINH , XEPLOAIHOCLUC, HOCKY, NAMHOC) values ('" + obj.MaHocSinh + "', '" + obj.XepLoaiHocLuc + "', '" + obj.HocKy + "', '" + obj.NamHoc + "')";
+            string strQuery = "insert into dbo.hocluc (MAHOCSINH , XEPLOAIHOCLUC, HOCKY, NAMHOC) values ('" + obj.MaHocSinh + "', N'" + obj.XepLoaiHocLuc + "', '" + obj.HocKy + "', '" + obj.NamHoc + "')";
             DataConfig config = new DataConfig();
             result = config.executeNoneQuery(strQuery);
             return result;
         }
         // check ID 
-        public bool CheckID(string _maHocSinh)
+        public bool CheckID(string _maHocSinh, string _hocKy, string _namHoc)
         {
             DataConfig config = new DataConfig();
-            string strQuery = "select * from dbo.hocluc where mahocsinh = '" + _maHocSinh + "'";
+            string strQuery = "select * from dbo.hocluc where mahocsinh = '" + _maHocSinh + "' and hocky = '" + _hocKy + "' and namhoc ='" + _namHoc + "' ";
             DataTable dt = new DataTable();
             dt = config.GetDataTable(strQuery);
             if (dt.Rows.Count > 0)
                 return true;
             return false;
         }
-
+        //kiem tra tong so dong co trong csdl
+        public int Count_Data_Rows()
+        {
+            int result = 0;
+            DataConfig config = new DataConfig();
+            string strQuery = "select * from dbo.hocluc ";
+            DataTable dt = new DataTable();
+            dt = config.GetDataTable(strQuery);
+            result = dt.Rows.Count;
+            return result;
+        }
         // cau lenh update
         public int Update(HocLucEntities obj)
         {
             int result = 0;
-            string strQuery = "update dbo.hocluc set mahocsinh = '" + obj.MaHocSinh + "', xeploaihocluc = '" + obj.XepLoaiHocLuc + "', hocky = '" + obj.HocKy + "', namhoc = '" + obj.NamHoc + "'";
+            string strQuery = "update dbo.hocluc set xeploaihocluc = N'" + obj.XepLoaiHocLuc + "', hocky = '" + obj.HocKy + "', namhoc = '" + obj.NamHoc + "' where  mahocsinh = '" + obj.MaHocSinh + "'";
             DataConfig config = new DataConfig();
             result = config.executeNoneQuery(strQuery);
             return result;
